49e5809d45bbedb0afa6ba5e37ae589889796680
Otacilio <otaciliogomesdesouza@gmail.com>
2015-03-24 14:08:37 -0300

    ola

--- src/main/java/com/mycompany/projetomarve/ola.java
+++ src/main/java/com/mycompany/projetomarve/ola.java
@@ -14,6 +14,7 @@ import javax.swing.JOptionPane;
 public class ola {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "hello world");
+        JOptionPane.showMessageDialog(null, "Ola mundo");
     }
     
 }
